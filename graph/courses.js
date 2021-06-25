5aa2eaed7b65c50f260a5ccf12d6e5497c56a5dd
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-24 22:09:44 -0400

    still working on dfs approach

--- graph/courses.js
+++ graph/courses.js
@@ -5,59 +5,27 @@
 //Graph DFS
 //Topographical Approach
 
-function canFinish(numCourses, prerequisites){
-  let adjacencyList = {}
-  for(let prerequisite of prerequisites){
-    if(!adjacencyList[prerequisite[0]]) adjacencyList[prerequisite[0]] = [prerequisite[1]]
-    else adjacencyList[prerequisite[0]].push(prerequisite[1])
+function canFinish(numCourses, prerequisites) {
+  const visited = {}
+  const adj = {}
+
+  for (let [u, v] of prerequisites) {
+    (!adj.hasOwnProperty(u)) ? adj[u] = [v]: adj[u].push(v)
   }
-  for(let course in adjacencyList){
-    for(let neighbor in adjacencyList[course]){
-      if(adjacency[adjacencyList[neighbor]])
+  console.log(adj)
+  dfs(prerequisites[0][0])
+  function dfs(vertex){
+    console.log(vertex)
+    if(vertex===null) return true
+    if(adj[vertex].forEach(node => visited.hasOwnProperty(node))) return false
+    visited[vertex] = true
+    console.log(visited)
+    for(let node of adj[vertex]){
+      if(!visited[node]) dfs(node)
     }
   }
+  return true
 }
 
-canFinish(3, [[1,0], [2,5], [3,1], [1,4], [1,6], [2,3], [3,4], [3,0]])
 
-// class Solution(object):
-//     def canFinish(self, numCourses, prerequisites):
-//         """
-//         :type numCourses: int
-//         :type prerequisites: List[List[int]]
-//         :rtype: bool
-//         """
-//         from collections import defaultdict
-//         courseDict = defaultdict(list)
-
-//         for relation in prerequisites:
-//             nextCourse, prevCourse = relation[0], relation[1]
-//             courseDict[prevCourse].append(nextCourse)
-
-//         path = [False] * numCourses
-//         for currCourse in range(numCourses):
-//             if self.isCyclic(currCourse, courseDict, path):
-//                 return False
-//         return True
-
-
-//     def isCyclic(self, currCourse, courseDict, path):
-//         """
-//         backtracking method to check that no cycle would be formed starting from currCourse
-//         """
-//         if path[currCourse]:
-//             # come across a previously visited node, i.e. detect the cycle
-//             return True
-
-//         # before backtracking, mark the node in the path
-//         path[currCourse] = True
-
-//         # backtracking
-//         ret = False
-//         for child in courseDict[currCourse]:
-//             ret = self.isCyclic(child, courseDict, path)
-//             if ret: break
-
-//         # after backtracking, remove the node from the path
-//         path[currCourse] = False
-//         return ret
+console.log(canFinish(2, [[1,0], [0,1]]))
